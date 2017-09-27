b7ba8d2ec62ecbe2396927254fccc719bd814dc5
Sree21 <sreekanth_telukuntla@yahoo.com>
2017-09-27 18:00:33 +0530

    removed user subcenter call in controllers

--- NMSReportingSuite/src/main/java/com/beehyv/nmsreporting/controller/LocationController.java
+++ NMSReportingSuite/src/main/java/com/beehyv/nmsreporting/controller/LocationController.java
@@ -156,7 +156,7 @@ public class LocationController {
     @RequestMapping(value = {"/subcenters/{blockId}"}, method = RequestMethod.GET)
     public @ResponseBody List<Subcenter> getsubcentersOfBlock(@PathVariable("blockId") Integer blockId) {
         User user = userService.getCurrentUser();
-        List<Subcenter> subcenters;
+        List<Subcenter> subcenters= new ArrayList<>();;
         if(user.getAccessLevel().equals(AccessLevel.NATIONAL.getAccessLevel())) {
             subcenters = locationService.getChildSubcenters(blockId);
         }
@@ -164,21 +164,14 @@ public class LocationController {
             if(locationService.findDistrictById(blockId).getStateOfDistrict().equals(user.getStateId())){
                 subcenters = locationService.getChildSubcenters(blockId);
             }
-            else{
-                subcenters = new ArrayList<>();
-            }
         }
         else if(user.getAccessLevel().equals(AccessLevel.DISTRICT.getAccessLevel())){
             subcenters = locationService.getChildSubcenters(blockId);
         }
-        else if(user.getAccessLevel().equals(AccessLevel.BLOCK.getAccessLevel())){
-            subcenters = new ArrayList<>();
+        else {
             subcenters =locationService.getChildSubcenters(user.getBlockId());
         }
-        else{
-            subcenters = new ArrayList<>();
-            subcenters.add(locationService.findSubcenterById(user.getSubcenterId()));
-        }
+
         return subcenters;
     }
 
