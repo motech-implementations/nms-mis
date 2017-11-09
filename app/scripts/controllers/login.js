775d7b0909a3a1dc9d04a618976a1f5ed6222769
sudheer <sudheer.reddy.b.s@gmail.com>
2017-11-09 14:03:42 +0530

    added captcha functionality to application

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,7 +1,7 @@
 (function(){
 	var nmsReportsApp = angular
 		.module('nmsReports')
-		.controller("LoginController", ['$scope', '$http', '$location','Captcha', function($scope, $http, $location, Captcha){
+		.controller("LoginController", ['$scope', '$http', '$location','Captcha','$mdDialog', function($scope, $http, $location, Captcha,$mdDialog){
 			
 			$scope.user = {};
 			$scope.user.rememberMe = false;
@@ -22,22 +22,75 @@
 				$scope.errorMessage = "Invalid Username/Password";
 			}
 
+//			$scope.login = function(){
+//				$http.post($scope.loginUrl,
+//					angular.toJson($scope.user),
+//					{
+//						headers: {
+//							'Content-Type': 'application/json'
+//						}
+//					})
+//					.success(function (data, status, headers, config) {
+//						console.log(data);
+//					})
+//					.error(function (data, status, header, config) {
+//						console.log(data);
+//					});
+//				}
+//			}
+
 			$scope.login = function(){
-				$http.post($scope.loginUrl,
-					angular.toJson($scope.user),
-					{
-						headers: {
-							'Content-Type': 'application/json'
-						}
-					})
-					.success(function (data, status, headers, config) {
-						console.log(data);
-					})
-					.error(function (data, status, header, config) {
-						console.log(data);
-					});
-				}
-			}
-		])
+
+			    if($scope.user.username == null){
+			        $scope.showAlert("Please specify a username")
+                    return;
+			    }
+			    if($scope.user.username == null){
+                    $scope.showAlert("Please specify a password")
+                    return;
+                }
+                if($scope.user.captchaCode == null){
+                    $scope.showAlert("Please enter the captchaCode")
+                    return;
+                }
+                if($scope.loginForm.captchaCode.$invalid){
+                    $scope.showAlert("Incorrect Captcha")
+                    return;
+                }
+
+                console.log($scope.user);
+                $http({
+                    method  : 'POST',
+                    url     : backend_root + 'nms/login',
+                    data    : $scope.user, //forms user object
+                    headers : {'Content-Type': 'application/json'}
+                }).then(function (result){
+                  console.log(result);
+
+                 },function (result){
+                    $scope.showAlert("error logging in ");
+                 });
+
+
+
+            }
+
+			$scope.showAlert = function(message) {
+                // Appending dialog to document.body to cover sidenav in docs app
+                // Modal dialogs should fully cover application
+                // to prevent interaction outside of dialog
+                $mdDialog.show(
+                  $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#popupContainer')))
+                    .clickOutsideToClose(true)
+                    .title('MIS Alert!!')
+                    .textContent(message)
+                    .ariaLabel('Alert Dialog Demo')
+                    .ok('Got it!')
+                );
+            }
+
+
+		}])
 })()
 
