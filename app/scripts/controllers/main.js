60c8886c0aace4fd9b34aaa4392afe21ff3cccbf
rahulbharadwaja <rahul.bharadwaja@beehyv.com>
2017-12-27 13:30:18 +0530

    changes for getting back to the same page when editing user

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,125 +1,122 @@
  (function(){
 	var nmsReportsApp = angular
 		.module('nmsReports')
-		.controller("MainController", ['$scope', '$state', '$http', 'UserFormFactory', function($scope, $state, $http, UserFormFactory){
+		.controller("MainController", ['$scope', '$state', '$http', '$localStorage', 'UserFormFactory', function($scope, $state, $http, $localStorage, UserFormFactory) {
 			
 			$scope.breadCrumbDict = {
 				'userManagement.userTable': [
 					{
 						'name': 'User Management',
 						'sref': 'userManagement.userTable({pageNum: 1})',
-						'active': false,
+						'active': false
 					}
 				],
 				'userManagement.createUser': [
 					{
 						'name': 'User Management',
 						'sref': 'userManagement.userTable({pageNum: 1})',
-						'active': true,
+						'active': true
 					},
 					{
 						'name': 'Create User',
 						'sref': 'userManagement.createUser',
-						'active': false,
-					},
+						'active': false
+					}
 				], 
 				'userManagement.editUser': [
 					{
 						'name': 'User Management',
 						'sref': 'userManagement.userTable({pageNum: 1})',
-						'active': true,
+						'active': true
 					},
 					{
 						'name': 'Edit User',
 						'sref': 'userManagement.editUser({pageNum: 1})',
-						'active': false,
-					},
+						'active': false
+					}
 				], 
 				'userManagement.bulkUpload': [
 					{
 						'name': 'User Management',
 						'sref': 'userManagement.userTable({pageNum: 1})',
-						'active': true,
+						'active': true
 					},
 					{
 						'name': 'Bulk Upload',
 						'sref': 'userManagement.bulkUpload',
-						'active': false,
-					},
+						'active': false
+					}
 				],
 				'profile': [
 					{
 						'name': 'Profile',
 						'sref': 'profile',
-						'active': false,
-					},
+						'active': false
+					}
 				],
 				'reports': [
 					{
 						'name': 'Reports',
 						'sref': 'reports',
-						'active': false,
-					},
+						'active': false
+					}
 				],
 				'login': [
 					{
 						'name': 'Login',
 						'sref': 'login',
-						'active': false,
-					},
+						'active': false
+					}
 				],
 				'forgotPassword': [
                 	{
                 		'name': 'ForgotPassword',
                 		'sref': 'forgotPassword',
-                		'active': false,
-                	},
+                		'active': false
+                	}
                 ],
                 'changePassword': [
                     {
                         'name': 'ChangePassword',
                         'sref': 'changePassword',
-                        'active': false,
-                    },
+                        'active': false
+                    }
                 ]
-			}
+			};
 
             $scope.child = {};
 
 			$scope.getBreadCrumb = function(state){
 				return $scope.breadCrumbDict[state];
-			}
+			};
 
 			$scope.getTitle = function(state){
-				var states = $scope.getBreadCrumb(state)
-				if(states != null){
+				var states = $scope.getBreadCrumb(state);
+				if (states !== null) {
 					return states[states.length - 1].name;
-				}
-				else{
+				} else {
 					return "Please Wait...";
 				}
-			}
+			};
+
+			$scope.goToUserTable = function() {
+			    delete $localStorage.filter;
+                if ($state.current.name !== 'userManagement.userTable') {
+                    $state.go('userManagement.userTable', {pageNum: 1});
+                }
+			};
 
 			$scope.activeTab = function(tabName){
-				if($state.current.name.indexOf(tabName) > -1){
-				    return true;
-				}
-				else
-				    return false;
-			}
+				return ($state.current.name.indexOf(tabName) > -1);
+			};
 
-			// $scope.breadCrumb = ['User Management', 'Create User'];
-			UserFormFactory.downloadCurrentUser()
-			.then(function(result){
+			UserFormFactory.downloadCurrentUser().then(function(result){
 				UserFormFactory.setCurrentUser(result.data);
 				$scope.currentUser = UserFormFactory.getCurrentUser();
-			})
+			});
 
 			$scope.logoutUrl = backend_root + "nms/logout";
-
-
-
-
 		}
 	])}
-)()
+
+)();
