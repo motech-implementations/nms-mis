cc066b173a296ce174f16e4959a3eb29aa4a8143
Chitra <chitrakumari@beehyv.com>
2018-03-27 18:28:05 +0530

    changed feedback form captcha

--- app/scripts/controllers/feedbackForm.js
+++ app/scripts/controllers/feedbackForm.js
@@ -54,55 +54,75 @@
                     }
                 }
 
-                if(!$scope.feedback.captchaCode){
+                if($scope.captchaResponse ==''){
                     if(UserFormFactory.isInternetExplorer()){
-                        alert("Please enter the captchaCode")
+                        alert("Check captcha")
                         return;
                     }
                     else{
-                        UserFormFactory.showAlert("Please enter the captchaCode")
+                        UserFormFactory.showAlert("Check captcha")
                         return;
                     }
+
+                }
+                else{
+                    var formElement = angular.element(e.target);
+                    formElement.attr("action", $scope.feedbackUrl);
+                    formElement.attr("method", "post");
+                    formElement[0].submit();
+
                 }
 
-                var captcha = new Captcha();
 
-                // captcha id for validating captcha at server-side
-                var captchaId = captcha.captchaId;
+//                if(!$scope.feedback.captchaCode){
+//                    if(UserFormFactory.isInternetExplorer()){
+//                        alert("Please enter the captchaCode")
+//                        return;
+//                    }
+//                    else{
+//                        UserFormFactory.showAlert("Please enter the captchaCode")
+//                        return;
+//                    }
+//                }
 
-                // captcha code input value for validating captcha at server-side
-                var captchaCode = angular.uppercase($scope.feedback.captchaCode);
-
-                var postData = {
-                  captchaId: captchaId,
-                  captchaCode: captchaCode
-                };
-
-                $http({
-                        method  : 'POST',
-                        url     : backend_root + 'nms/captcha',
-                        data    : postData, //forms user object
-                        headers : {'Content-Type': 'application/json'}
-                    }).then(function(response){
-                         if(response.data.success){
-                             var formElement = angular.element(e.target);
-                             formElement.attr("action", $scope.feedbackUrl);
-                             formElement.attr("method", "post");
-                             formElement[0].submit();
-                         }
-                         else{
-                             if(UserFormFactory.isInternetExplorer()){
-                                 alert("Incorrect Captcha")
-                                 return;
-                             }
-                             else{
-                                 UserFormFactory.showAlert("Incorrect Captcha")
-                                 return;
-                             }
-
-                         }
-
-                     })
+//                var captcha = new Captcha();
+//
+//                // captcha id for validating captcha at server-side
+//                var captchaId = captcha.captchaId;
+//
+//                // captcha code input value for validating captcha at server-side
+//                var captchaCode = angular.uppercase($scope.feedback.captchaCode);
+//
+//                var postData = {
+//                  captchaId: captchaId,
+//                  captchaCode: captchaCode
+//                };
+//
+//                $http({
+//                        method  : 'POST',
+//                        url     : backend_root + 'nms/captcha',
+//                        data    : postData, //forms user object
+//                        headers : {'Content-Type': 'application/json'}
+//                    }).then(function(response){
+//                         if(response.data.success){
+//                             var formElement = angular.element(e.target);
+//                             formElement.attr("action", $scope.feedbackUrl);
+//                             formElement.attr("method", "post");
+//                             formElement[0].submit();
+//                         }
+//                         else{
+//                             if(UserFormFactory.isInternetExplorer()){
+//                                 alert("Incorrect Captcha")
+//                                 return;
+//                             }
+//                             else{
+//                                 UserFormFactory.showAlert("Incorrect Captcha")
+//                                 return;
+//                             }
+//
+//                         }
+//
+//                     })
 
 
 		    }
