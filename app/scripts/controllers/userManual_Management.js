ed4d69be3bc756b43faf44579363586712e3c9c8
ashooues <spicyashish@gmail.com>
2018-03-27 22:01:48 +0530

    user manual mionor change

--- app/scripts/controllers/userManual_Management.js
+++ app/scripts/controllers/userManual_Management.js
@@ -2,13 +2,7 @@
 	var nmsReportsApp = angular
 		.module('nmsReports')
 		.controller("userManual_ManagementController", ['$scope', '$state', 'UserFormFactory', function($scope, $state, UserFormFactory){
-/*
-			UserFormFactory.isLoggedIn()
-			.then(function(result){
-				if(!result.data){
-					$state.go('login', {});
-				}
-			})*/
+
 
 		}])
 })();
