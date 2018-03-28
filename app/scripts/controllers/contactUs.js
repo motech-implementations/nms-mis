78278b731a21c385f94c7b1817ad1106c55fad50
Chitra <chitrakumari@beehyv.com>
2018-03-28 14:00:29 +0530

    Correction of issue no. D-012 - Submit button position in ContactUs page

--- app/scripts/controllers/contactUs.js
+++ app/scripts/controllers/contactUs.js
@@ -3,43 +3,20 @@
 		.module('nmsReports')
 		.controller("ContactUsController", ['$scope', '$state', 'UserFormFactory','$http', '$location', function($scope, $state,$location,$http, UserFormFactory){
 
-//			UserFormFactory.isAdminLoggedIn()
-//			.then(function(result){
-//				if(!result.data){
-//					$state.go('login', {});
-//				}
-//			})
-
             $scope.contactUsUrl = backend_root + 'nms/mail/sendEmailForContactUs';
 
-//            var url = $location.absUrl();
-//            var error = url.split('?')[1]
-//            $scope.preUrl = "";
-//            $scope.errorMessage = "";
-//            if(error == null){
-//                $scope.errorMessage = "";
-//            }
-//            else{
-//                $scope.errorMessage = "Invalid data";
-//            }
             $scope.contactUs = {};
-
-
-
 
             $scope.contactUs = function(e){
 
-                     var formElement = angular.element(e.target);
-                     formElement.attr("action", $scope.contactUsUrl);
-                     formElement.attr("method", "post");
-                     formElement[0].submit();
-
-		}
 
 
 
 
-
-
+                var formElement = angular.element(e.target);
+                formElement.attr("action", $scope.contactUsUrl);
+                formElement.attr("method", "post");
+                formElement[0].submit();
+		    }
 		}])
 })();
