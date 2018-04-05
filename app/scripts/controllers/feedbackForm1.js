f7216cf082d90f80b8c2a6e020fca6ce95d6fcab
Chitra <chitrakumari@beehyv.com>
2018-04-05 14:55:48 +0530

    Changed in sendFeedback method and sendEmailForContactUs method

--- app/scripts/controllers/feedbackForm1.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var nmsReportsApp = angular
-        .module('nmsReports');
-
-    nmsReportsApp.controller("FeedbackForm1Controller", ['$scope', '$state', 'UserFormFactory','$http', '$location','Captcha', function($scope, $state, UserFormFactory,$http,$location,Captcha){
-
-    }])
-
-})
