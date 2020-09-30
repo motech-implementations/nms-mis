350e599cfbc991f4f50449dc90f931d91c03e21f
jangidrakesh <jangidrakesh1996@gmail.com>
2020-09-30 15:52:18 +0530

    grunting html pages

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
            dist: {
              files: {
                'dist/app1.js': [ 'dist/app1.js' ],
-               'dist/app2.js': [ 'dist/app2.js' ]
+               'dist/app2.js': [ 'dist/app2.js' ],
              },
              options: {
                mangle: false
@@ -32,23 +32,6 @@ module.exports = function(grunt) {
           src: ['scripts/factories/auth.js', 'scripts/factories/exportuiGrid.js', 'scripts/factories/userTable.js', 'bower_components/cryptojslib/rollups/aes.js', 'bower_components/angular-cryptography/mdo-angular-cryptography.js'],
           dest: 'dist/app2.js'
       },
-//      vendor1: {
-//          src: ['bower_components/angular/angular.min.js',
-//          'bower_components/angular-animate/angular-animate.min.js',
-//          'bower_components/angular-ui-carousel/dist/ui-carousel.min.js'],
-//          dest: 'dist/vendor1.js'
-//      },
-//      vendor2: {
-//            src: ['bower_components/angular-ui-router/release/angular-ui-router.min.js',
-//            'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js','bower_components/angular-messages/angular-messages.min.js',
-//            'bower_components/angular-ui-grid/ui-grid.min.js', 'bower_components/ng-device-detector/ng-device-detector.min.js',
-//            'bower_components/angular-ui-validate/dist/validate.min.js','bower_components/angular-aria/angular-aria.min.js','bower_components/re-tree/re-tree.min.js',
-//            'bower_components/ngstorage/ngStorage.min.js','bower_components/ng-file-upload/ng-file-upload-shim.min.js',
-//            'bower_components/angular-cryptography/mdo-angular-cryptography.js', 'bower_components/ng-file-upload/ng-file-upload.min.js',
-//             'bower_components/pdfmake/build/pdfmake.min.js','bower_components/angular-idle-service/dist/angular-idle-service.js',
-//             'bower_components/pdfmake/build/vfs_fonts.js', 'bower_components/cryptojslib/rollups/aes.js'],
-//            dest: 'dist/vendor2.js'
-//       },
 
       css : {
           src: [ 'bower_components/bootstrap/dist/css/bootstrap.min.css','bower_components/angular-material/angular-material.min.css',
@@ -59,24 +42,15 @@ module.exports = function(grunt) {
 
     },
 
-
-//
-//    watch: {
-//      dev: {
-//        files: [ 'Gruntfile.js','scripts/**/*.js','/scripts/*.js'],
-//        tasks: [ 'concat:dist',],
-//        options: {
-//          atBegin: true
-//        }
-//      },
-//      min: {
-//        files: [ 'Gruntfile.js','scripts/**/*.js','/scripts/*.js'],
-//        tasks: [ 'concat:dist','uglify:dist' ],
-//        options: {
-//          atBegin: true
-//        }
-//      }
-//    }
+    html2js: {
+      options: {
+        // custom options, see below
+      },
+      main: {
+        src: ['views/*.html'],
+        dest: 'dist/templates.js'
+      },
+    },
 
   });
 
@@ -92,7 +66,8 @@ module.exports = function(grunt) {
   grunt.registerTask('dev', [ 'bower', 'watch:dev' ]);
   grunt.registerTask('test', [ 'bower']);
   grunt.registerTask('minified', [ 'bower', 'watch:min' ]);
-  grunt.registerTask('package', [ 'concat:dist1', 'concat:dist2', 'uglify:dist']);
+  grunt.registerTask('package', [ 'concat:dist1', 'concat:dist2','html2js:main', 'uglify:dist']);
+
 
   // Loading of tasks and registering tasks will be written here
 };
