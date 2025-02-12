574735cb714b1ce0ef37d46c06a09893f3f95d6f
dinesh.c <dinesh.c@beehyv.com>
2025-02-12 15:07:15 +0530

    Fixed past days data in today's file in case of failure

--- NMSReportingSuite/src/main/java/com/beehyv/nmsreporting/dao/impl/TargetFileAuditDaoImpl.java
+++ NMSReportingSuite/src/main/java/com/beehyv/nmsreporting/dao/impl/TargetFileAuditDaoImpl.java
@@ -39,11 +39,11 @@ public class TargetFileAuditDaoImpl extends AbstractDao<Integer, SmsFileAuditLog
     @Override
     @Transactional
     public boolean hasFailureOnDate(Date date) {
+        if (date == null) {
+            throw new IllegalArgumentException("Date cannot be null");
+        }
 
-        Criteria criteria = createEntityCriteria();
-        criteria.add(Restrictions.eq("successStatus", false));
-
-        // Set date range for the specific day
+        // Set date range for the specific day.
         Calendar startOfDay = Calendar.getInstance();
         startOfDay.setTime(date);
         startOfDay.set(Calendar.HOUR_OF_DAY, 0);
@@ -54,11 +54,28 @@ public class TargetFileAuditDaoImpl extends AbstractDao<Integer, SmsFileAuditLog
         Calendar endOfDay = (Calendar) startOfDay.clone();
         endOfDay.add(Calendar.DAY_OF_MONTH, 1);
 
+        // Build criteria for all audit logs on the given day regardless of successStatus.
+        Criteria criteria = createEntityCriteria();
         criteria.add(Restrictions.ge("createdAt", startOfDay.getTime()));
         criteria.add(Restrictions.lt("createdAt", endOfDay.getTime()));
 
         List<SmsFileAuditLog> logs = criteria.list();
-        return logs != null && !logs.isEmpty();
+
+        // If no logs exist for that day, there's no failure.
+        if (logs == null || logs.isEmpty()) {
+            return true;
+        }
+
+        // If any log on that day indicates success
+        // then we return false because the day is not considered as having a failure.
+        for (SmsFileAuditLog log : logs) {
+            if (Boolean.TRUE.equals(log.getSuccessStatus())) {
+                return false;
+            }
+        }
+
+        // If all entries are failures then we return true.
+        return true;
     }
 
 
